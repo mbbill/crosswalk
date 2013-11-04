06e74f7ccf4a852ebf6ef434337b41f2623fc475
Ming,Bai <bai.ming@intel.com>
2013-11-04 20:05:38 +0800

    Added D-Bus implementation for Linux as well as a launcher

--- application/common/constants.cc
+++ application/common/constants.cc
@@ -16,5 +16,25 @@ const base::FilePath::CharType kMessagesFilename[] =
 const char kGeneratedMainDocumentFilename[] =
     "_generated_main_document.html";
 
+// Launcher switches.
+const char kSwitchLaunch[] =
+    "launch";
+const char kSwitchInstall[] =
+    "install";
+const char kSwitchUninstall[] =
+    "uninstall";
+const char kSwitchListApps[] =
+    "list-apps";
+const char kSwitchVerbose[] =
+    "verbose";
+
+// D-Bus constants.
+extern const char kDbusServiceName[] =
+    "org.crosswalkproject.service";
+extern const char kDbusObjectPath[] =
+    "/org/crosswalkproject/service";
+extern const char kDbusAppInterfaceName[] =
+    "org.crosswalkproject.application";
+
 }  // namespace application
 }  // namespace xwalk
